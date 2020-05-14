6348552a157b7e4be98ddbaf077bcd08a3cd73db
Layouwen <Layouwen@gmail.com>
2020-05-14 21:44:31 +0800

    add touch model adaptation Mobile

--- README.md
+++ README.md
@@ -2,10 +2,12 @@
 
 ## 查看效果
 
+[点击预览](https://layouwen.github.io/Art-Time/index.html)
+
 ## 介绍
 
 ## 联系方式
 
 > 作者：梁又文  
 > QQ：794234293  
-> 微信：gdgzyw  
+> 微信：gdgzyw
--- index.js
+++ index.js
@@ -1,14 +1,37 @@
-// 监听鼠标事件
-canvas.onclick = (e) => {
-    let div = document.createElement('div')
-    div.style.position = 'absolute'
-    div.style.left = e.clientX + 'px'
-    div.style.top = e.clientY + 'px'
-    div.style.marginTop = '-3px'
-    div.style.marginLeft = '-3px'
-    div.style.width = '6px'
-    div.style.height = '6px'
-    div.style.borderRadius = '50%'
-    div.style.backgroundColor = 'black'
-    canvas.appendChild(div)
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+canvas.width = document.documentElement.clientWidth
+canvas.height = document.documentElement.clientHeight
+var isTouchDevice = 'ontouchstart' in document.documentElement
+let painting = false
+
+if (isTouchDevice) {
+    canvas.ontouchstart = () => {
+        painting = true
+    }
+    canvas.ontouchend = () => {
+        painting = false
+    }
+    canvas.ontouchmove = (e) => {
+        if (painting === true) {
+            ctx.beginPath()
+            // 手机端触摸事件有可能存在多点，我们需要获取第一个点的坐标 touches
+            ctx.arc(e.touches[0].clientX, e.touches[0].clientY, 3, 0, 2 * Math.PI)
+            ctx.fill()
+        }
+    }
+} else {
+    canvas.onmousedown = () => {
+        painting = true
+    }
+    canvas.onmouseup = () => {
+        painting = false
+    }
+    canvas.onmousemove = (e) => {
+        if (painting === true) {
+            ctx.beginPath()
+            ctx.arc(e.clientX, e.clientY, 3, 0, 2 * Math.PI)
+            ctx.fill()
+        }
+    }
 }
