9711f20ecd4165cd0479e12e11753f86bf85b8ba
YouwenLiang <layouwen@gmail.com>
2021-03-25 15:47:10 +0800

    format

--- README.md
+++ README.md
@@ -2,13 +2,14 @@
 
 ## 查看效果
 
-[点击预览](https://layouwen.github.io/Art-Time/index.html)
+[点击预览](https://layouwen.github.io/art-time/)
 
 ## 介绍
 
-适合初学Canvas的朋友参考，本项目使用 canvas、原生JS 实现简易画板的效果。可用于日常学习及草稿等，适配 PC端、移动端。
+适合初学 Canvas 的朋友参考，本项目使用 canvas、原生 JS 实现简易画板的效果。可用于日常学习及草稿等，适配 PC 端、移动端。
 
 该画板的功能有：
+
 - 三种颜色的画笔
 - 三种画笔粗细
 - 清空/重置画板
--- index.js
+++ index.js
@@ -23,119 +23,119 @@ let bgcWhite = document.querySelector('.bgcWhite')
 let penEraser = document.querySelector('.penEraser')
 
 penBlack.onclick = () => {
-    ctx.strokeStyle !== 'black' ? ctx.strokeStyle = 'black' : ctx.strokeStyle
+  ctx.strokeStyle !== 'black' ? ctx.strokeStyle = 'black' : ctx.strokeStyle
 }
 penWhite.onclick = () => {
-    ctx.strokeStyle !== 'white' ? ctx.strokeStyle = 'white' : ctx.strokeStyle
+  ctx.strokeStyle !== 'white' ? ctx.strokeStyle = 'white' : ctx.strokeStyle
 }
 penRed.onclick = () => {
-    ctx.strokeStyle !== 'red' ? ctx.strokeStyle = 'red' : ctx.strokeStyle
+  ctx.strokeStyle !== 'red' ? ctx.strokeStyle = 'red' : ctx.strokeStyle
 }
 
 penSmall.onclick = () => {
-    ctx.lineWidth !== 2 ? ctx.lineWidth = 2 : ctx.lineWidth
+  ctx.lineWidth !== 2 ? ctx.lineWidth = 2 : ctx.lineWidth
 }
 penMiddle.onclick = () => {
-    ctx.lineWidth !== 4 ? ctx.lineWidth = 4 : ctx.lineWidth
+  ctx.lineWidth !== 4 ? ctx.lineWidth = 4 : ctx.lineWidth
 }
 penBig.onclick = () => {
-    ctx.lineWidth !== 8 ? ctx.lineWidth = 8 : ctx.lineWidth
+  ctx.lineWidth !== 8 ? ctx.lineWidth = 8 : ctx.lineWidth
 }
 
 bgcBlack.onclick = () => {
-    canvas.style.background !== 'black' ? canvas.style.background = 'black' : canvas.style.background
+  canvas.style.background !== 'black' ? canvas.style.background = 'black' : canvas.style.background
 }
 bgcWhite.onclick = () => {
-    canvas.style.background !== 'White' ? canvas.style.background = 'White' : canvas.style.background
+  canvas.style.background !== 'White' ? canvas.style.background = 'White' : canvas.style.background
 }
 
 penEraser.onclick = () => {
-    resetBoard()
+  resetBoard()
 }
 
 // 绘画
 
 if (isTouchDevice) {
-    canvas.ontouchstart = (e) => {
-        last = [e.touches[0].clientX, e.touches[0].clientY]
-    }
-    canvas.ontouchmove = (e) => {
-        // 手机端触摸事件有可能存在多点，我们需要获取第一个点的坐标 touches
-        draw(last[0], last[1], e.touches[0].clientX, e.touches[0].clientY)
-        last = [e.touches[0].clientX, e.touches[0].clientY]
-    }
+  canvas.ontouchstart = (e) => {
+    last = [e.touches[0].clientX, e.touches[0].clientY]
+  }
+  canvas.ontouchmove = (e) => {
+    // 手机端触摸事件有可能存在多点，我们需要获取第一个点的坐标 touches
+    draw(last[0], last[1], e.touches[0].clientX, e.touches[0].clientY)
+    last = [e.touches[0].clientX, e.touches[0].clientY]
+  }
 } else {
-    canvas.onmousedown = (e) => {
-        painting = true
-        last = [e.clientX, e.clientY]
+  canvas.onmousedown = (e) => {
+    painting = true
+    last = [e.clientX, e.clientY]
+  }
+  canvas.onmouseup = () => {
+    painting = false
+  }
+  canvas.onmousemove = (e) => {
+    if (painting === true) {
+      draw(last[0], last[1], e.clientX, e.clientY)
+      last = [e.clientX, e.clientY]
     }
-    canvas.onmouseup = () => {
-        painting = false
-    }
-    canvas.onmousemove = (e) => {
-        if (painting === true) {
-            draw(last[0], last[1], e.clientX, e.clientY)
-            last = [e.clientX, e.clientY]
-        }
-    }
+  }
 }
 
 // 检测窗口是否变动
 let onresizeTime = 0
 if (isTouchDevice) {
-    window.addEventListener("orientationchange", function () {
-        if (confirm('您的窗口发生改变，请问是否重置画板')) {
-            resetBoard()
-        }
-    }, false);
-} else {
-    window.onresize = () => {
-        if (onresizeTime !== 1) {
-            if (confirm('您的窗口发生改变，请问是否重置画板')) {
-                setTimeout(() => {
-                    resetBoard()
-                }, 200)
-                onresizeTime = 1
-            } else {
-                onresizeTime = 1
-            }
-        } else {
-            onresizeTime = 0
-        }
+  window.addEventListener("orientationchange", function () {
+    if (confirm('您的窗口发生改变，请问是否重置画板')) {
+      resetBoard()
     }
+  }, false);
+} else {
+  window.onresize = () => {
+    if (onresizeTime !== 1) {
+      if (confirm('您的窗口发生改变，请问是否重置画板')) {
+        setTimeout(() => {
+          resetBoard()
+        }, 200)
+        onresizeTime = 1
+      } else {
+        onresizeTime = 1
+      }
+    } else {
+      onresizeTime = 0
+    }
+  }
 }
 
 
 
 // 画板重置
 function resetBoard() {
-    if (canvas.style.background === 'white') {
-        canvas.width = document.documentElement.clientWidth
-        canvas.height = document.documentElement.clientHeight - 4
-        ctx.lineWidth = 4
-        ctx.lineCap = "round"
-        ctx.strokeStyle = 'black'
-    } else {
-        canvas.width = document.documentElement.clientWidth
-        canvas.height = document.documentElement.clientHeight - 4
-        ctx.lineWidth = 4
-        ctx.lineCap = "round"
-        ctx.strokeStyle = 'white'
-    }
+  if (canvas.style.background === 'white') {
+    canvas.width = document.documentElement.clientWidth
+    canvas.height = document.documentElement.clientHeight - 4
+    ctx.lineWidth = 4
+    ctx.lineCap = "round"
+    ctx.strokeStyle = 'black'
+  } else {
+    canvas.width = document.documentElement.clientWidth
+    canvas.height = document.documentElement.clientHeight - 4
+    ctx.lineWidth = 4
+    ctx.lineCap = "round"
+    ctx.strokeStyle = 'white'
+  }
 }
 
 // 声明一个划线的函数
 function draw(x1, x2, y1, y2) {
-    ctx.beginPath();
-    ctx.moveTo(x1, x2);
-    ctx.lineTo(y1, y2);
-    ctx.stroke()
+  ctx.beginPath();
+  ctx.moveTo(x1, x2);
+  ctx.lineTo(y1, y2);
+  ctx.stroke()
 }
 
 // 禁止页面滑动
 var mo = function (e) {
-    e.preventDefault();
+  e.preventDefault();
 }
 document.addEventListener("touchmove", mo, {
-    passive: false
+  passive: false
 })
